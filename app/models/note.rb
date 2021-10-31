1130d4e88a892dcb90f0efa842b3757ea4518cbd
pedro <pedropablo.yanez@hotmail.com>
2021-10-31 20:49:20 -0300

    add new model note

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,17 +1,17 @@
 # == Schema Information
 #
-# Table name: participants
+# Table name: notes
 #
 #  id         :bigint           not null, primary key
-#  role       :integer
+#  body       :text
 #  user_id    :bigint           not null
 #  task_id    :bigint           not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
-class Participant < ApplicationRecord
+class Note < ApplicationRecord
   belongs_to :user
   belongs_to :task
 
-  validates_uniqueness_of  :user_id,scope: [:task_id],  allow_blank: false, message: "participants cant be repeated in same task"
+  validates :body, presence: true
 end
